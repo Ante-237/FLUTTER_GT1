ed7e970424594e563f6c4ba6b697a727b10e0bc2
izzeddin62 <i.ishimwe@alustudent.com>
2023-09-25 16:41:05 +0200

    add feedback screen

---
+++ my_appT1/lib/ui/feedback_input.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FeedbackInput extends StatelessWidget {
+  final String? labelText;
+  final String? hintText;
+
+  FeedbackInput({this.labelText, this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (labelText != null)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              labelText!,
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        SizedBox(
+            height: 50.0,
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.only(left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(3.0),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: hintText ?? 'Enter...',
+                  ),
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+}

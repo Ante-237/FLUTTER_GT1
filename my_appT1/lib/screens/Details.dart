6a6868510b11ec400868f642af3a0ab548936c54
izzeddin62 <i.ishimwe@alustudent.com>
2023-09-26 11:18:41 +0200

    remove padding right and left on the add feedback button container

--- my_appT1/lib/screens/Details.dart
+++ my_appT1/lib/screens/Details.dart
@@ -3,7 +3,7 @@ import '../ui/feedback_input.dart';
 import '../ui/item.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  const DetailsScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,98 +17,106 @@ class DetailsScreen extends StatelessWidget {
         ),
         title: const Text('Details'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16.0),
-            Center(
-              child: Image.asset(
-                'assets/logo.png',
-                width: 400.0,
-                height: 400.0,
-                fit: BoxFit.cover,
-              ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16.0),
+          Center(
+            child: Image.asset(
+              'assets/logo.png',
+              width: 400.0,
+              height: 400.0,
+              fit: BoxFit.cover,
             ),
-            // const SizedBox(height: 1.0),
-            const Text(
-              'Description',
-              style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey),
-            ),
-            const SizedBox(height: 2.0),
-            const Text(
-              'This is just test material',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            const Text(
-              'description description',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.center,
-              child: ElevatedButton.icon(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(
-                    const EdgeInsets.only(
-                        left: 16.0,
-                        right: 16.0,
-                        top: 14.0,
-                        bottom: 14.0), // Adjust the padding as needed
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 16.0), // Padding for other elements
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Description',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
                   ),
                 ),
-                onPressed: () {},
-                icon: const Icon(Icons.add),
-                label: const Text(
-                  'Save',
-                  style: TextStyle(fontSize: 20.0),
+                SizedBox(height: 2.0),
+                Text(
+                  'This is just test material',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                Text(
+                  'description description',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton.icon(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  const EdgeInsets.only(
+                    left: 20.0,
+                    right: 20.0,
+                    top: 16.0,
+                    bottom: 16.0,
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text(
+                'Save',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          SizedBox(
+            height: 80.0,
+            width: double.infinity,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, -3),
+                  ),
+                ],
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: ElevatedButton.icon(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.only(
+                        top: 16.0,
+                        bottom: 16.0,
+                        right: 32.0,
+                        left: 32.0,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text(
+                    'ADD FEEDBACK',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
-            SizedBox(
-                height: 80.0,
-                width: double.infinity,
-                child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, -3), // Negative offset for shadow at the top
-                        ),
-                      ],
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: ElevatedButton.icon(
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.only(
-                                left: 32.0,
-                                right: 32.0,
-                                top: 16.0,
-                                bottom: 16.0), // Adjust the padding as needed
-                          ),
-                        ),
-                        onPressed: () {
-                          // Add your ADD FEEDBACK button action here
-                        },
-                        icon: const Icon(Icons.add),
-                        label: const Text(
-                          'ADD FEEDBACK',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      ),
-                    ))),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

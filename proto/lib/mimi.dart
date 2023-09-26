1e69263792d2e9f5d3994bb3a2e596fa4f70dff0
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-09-26 15:18:02 +0200

    Commit

---
+++ proto/lib/mimi.dart
@@ -0,0 +1,356 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CoverPage(),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFF27DCFC)),
+    );
+  }
+}
+
+class CoverPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Add the app logo at the top-center
+              Image.asset(
+                'Assets/Images/app_logo.png', // Replace with your app logo asset path
+                width: 300.0,
+                height: 300.0,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Go to app for slangs in local areas compared to your previous locations',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to the Login page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text('Log In'),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'or',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () {
+                  // Navigate to the Signup page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupPage()),
+                  );
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Add the app logo at the top-center
+              Image.asset(
+                'Assets/Images/app_logo.png', // Replace with your app logo asset path
+                width: 300.0,
+                height: 300.0,
+              ),
+              SizedBox(height: 5.0),
+              Column(
+                children: <Widget>[
+                  // Email Field
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Email'),
+                      SizedBox(height: 5.0), // Add some spacing
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email', // Placeholder text
+                          filled: true, // Fill the background
+                          fillColor: Colors.white, // Background color
+                          border:
+                              OutlineInputBorder(), // Add rectangular border
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  // Password Field
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Password'),
+                      SizedBox(height: 5.0), // Add some spacing
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password', // Placeholder text
+                          filled: true, // Fill the background
+                          fillColor: Colors.white, // Background color
+                          border:
+                              OutlineInputBorder(), // Add rectangular border
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement your login logic here
+                },
+                child: Text('Login'),
+              ),
+              SizedBox(height: 15.0), // Add some spacing between the buttons
+              Column(
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      // Implement the forgot password functionality
+                    },
+                    child: Text('Forgot Password?'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Navigate to the Signup page
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignupPage()),
+                      );
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Add the app logo at the top-center
+              Image.asset(
+                'Assets/Images/app_logo.png', // Replace with your app logo asset path
+                width: 300.0,
+                height: 300.0,
+              ),
+              SizedBox(height: 5.0),
+              Column(
+                children: <Widget>[
+                  // Email Field (for Signup)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Email'),
+                      SizedBox(height: 5.0), // Add some spacing
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email', // Placeholder text
+                          filled: true, // Fill the background
+                          fillColor: Colors.white, // Background color
+                          border:
+                              OutlineInputBorder(), // Add rectangular border
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  // Password Field (for Signup)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Password'),
+                      SizedBox(height: 5.0), // Add some spacing
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password', // Placeholder text
+                          filled: true, // Fill the background
+                          fillColor: Colors.white, // Background color
+                          border:
+                              OutlineInputBorder(), // Add rectangular border
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement signup logic here
+                  // Navigate to the Create Profile page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CreateProfilePage()),
+                  );
+                },
+                child: Text('Sign Up'),
+              ),
+              SizedBox(height: 15.0), // Add some spacing between the buttons
+              Column(
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      // Navigate back to the Login page
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    child: Text('Already have an account? Login'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CreateProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Profile Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Profile Picture Section
+              Container(
+                width: 200.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 10.0,
+                  ),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add_a_photo,
+                    size: 120.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              // Full Name Section
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Full Name'),
+                  SizedBox(height: 5.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Full Name',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              // Save Profile Button
+              ElevatedButton(
+                onPressed: () {
+                  // Implement save profile logic here
+                },
+                child: Text('Save Profile'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

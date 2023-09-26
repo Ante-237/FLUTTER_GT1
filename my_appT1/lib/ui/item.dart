3979adc756bd5ac7000cd801e8d1e2253ce9e7cf
izzeddin62 <i.ishimwe@alustudent.com>
2023-09-26 10:11:35 +0200

    add feedback page

---
+++ my_appT1/lib/ui/item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Item extends StatelessWidget {
+  final String title;
+  final String description;
+
+  Item({
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey.withOpacity(0.5),
+            width: 1.0,
+          ),
+
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipOval(
+              child: Container(
+            color: Colors.grey,
+            child: Image.asset(
+              'logo.png',
+              width: 50.0,
+              height: 50.0,
+              fit: BoxFit.cover,
+            ),
+          )),
+          const SizedBox(width: 16.0),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF555555),
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(
+                    height:
+                        4.0), // Add vertical spacing between title and description
+                Text(
+                  description,
+                  style:
+                      const TextStyle(fontSize: 14.0, color: Color(0xFF666666)),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
